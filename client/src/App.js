45d7a2ca635e7060da97e6911aba102a47cdd9c1
Aleksanteri Heliövaara <aleksanteriheliovaara@aleksanterismbp.lan>
2019-06-25 15:39:09 +0300

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
       conversation: [...this.state.conversation, msg],
     });
 
-    fetch('/api', {
+    fetch('http://localhost:5000/api', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
